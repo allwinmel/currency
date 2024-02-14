09ac6ced6cf0d25efa9ff132c1ffb8183088d0b5
Allwin Mel <allwinmel@gmail.com>
2024-02-14 18:02:56 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios  from 'axios';
+
+function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [exchangeRate, setExchangeRate] = useState(null); 
+   useEffect(() => {
+     const getExchangeRate = async () => {
+           try{
+             let url = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+
+             const response = await axios.get(url);
+            //  console.log(response);
+             setExchangeRate(response.data.rates[toCurrency]);
+           }catch(error) {
+            console.log("Error fetching exchange rate:", error);
+           }   
+     };
+     getExchangeRate();
+   },[fromCurrency, toCurrency]);
+
+   useEffect (() =>{
+    if (exchangeRate !== null) {
+      setConvertedAmount ((amount * exchangeRate).toFixed(2));
+    }
+   },[amount, exchangeRate]);
+
+  const handleAmountChange = (e) => {
+      const value = parseFloat(e.target.value);
+      setAmount(isNaN(value)? 0 : value);
+  };
+
+   const handleFromCurrentChange = (e) => {
+    setFromCurrency(e.target.value);
+   };
+
+   const handleToCurrentChange = (e) => {
+    setToCurrency(e.target.value);
+   };
+
+
+  return (
+    <>
+    <div className='currency-converter'>
+     <div className='box'></div> 
+     <div className="data">
+      <h1>Currency Converter</h1>
+      <div className="input-container">
+        <label htmlFor='amt'>Amount:</label>
+        <input type='number' id='amt' value={amount} onChange={handleAmountChange} /> 
+      </div>
+      <div className="input-container">
+        <label htmlFor='fromCurrency'>From Currency:</label>
+        <select  id="fromCurrency" value={fromCurrency} onChange={handleFromCurrentChange} >
+        <option value="USD">USD - United States Dollar</option>
+        <option value="EUR">EUR - Euro</option>
+        <option value="JPY">JPY - Japanese Yen</option>
+        <option value="GBP">GBP - British Pound Sterling</option>
+        <option value="AUD">AUD - Australian Dollar</option>
+        <option value="CAD">CAD - Canadian Dollar</option>
+        <option value="CHF">CHF - Swiss Franc</option>
+        <option value="CNY">CNY - Chinese Yuan</option>
+        <option value="DKK">DKK - Danish Krone</option>
+        <option value="HKD">HKD - Hong Kong Dollar</option>
+        <option value="IDR">IDR - Indonesian Rupiah</option>
+        <option value="ILS">ILS - Israeli New Shekel</option>
+        <option value="INR">INR - Indian Rupee</option>
+        </select>
+      </div>
+      <div className="input-container">
+        <label htmlFor='toCurrency'>To Currency:</label>
+        <select  id="toCurrency" value={toCurrency} onChange={handleToCurrentChange} >
+        <option value="USD">USD - United States Dollar</option>
+        <option value="EUR">EUR - Euro</option>
+        <option value="JPY">JPY - Japanese Yen</option>
+        <option value="GBP">GBP - British Pound Sterling</option>
+        <option value="AUD">AUD - Australian Dollar</option>
+        <option value="CAD">CAD - Canadian Dollar</option>
+        <option value="CHF">CHF - Swiss Franc</option>
+        <option value="CNY">CNY - Chinese Yuan</option>
+        <option value="DKK">DKK - Danish Krone</option>
+        <option value="HKD">HKD - Hong Kong Dollar</option>
+        <option value="IDR">IDR - Indonesian Rupiah</option>
+        <option value="ILS">ILS - Israeli New Shekel</option>
+        <option value="INR">INR - Indian Rupee</option>
+        </select>
+      </div>
+      <div className="result">
+        <p> {amount} {fromCurrency} is equal  to  
+        {convertedAmount} {toCurrency} </p>
+      </div>
+     </div>
+    </div>
+    </>
+  )
+}
+
+export default App
